29afe730de7c8993cc39ebb1a56b2084c68dbdff
Jefferson Lee <jefferson@surgehq.ai>
2023-06-20 16:27:50 -0700

    more debugging

--- lib/ld-eventsource/client.rb
+++ lib/ld-eventsource/client.rb
@@ -340,7 +340,8 @@ module SSE
         end
       end
       @logger.info { "chunks #{chunks}" }
-      event_parser = Impl::EventParser.new(Impl::BufferedLineReader.lines_from(chunks), @last_id)
+      # event_parser = Impl::EventParser.new(Impl::BufferedLineReader.lines_from(chunks), @last_id)
+      event_parser = Impl::BasicEventParser.new(chunks)
 
       event_parser.items.each do |item|
         return if @stopped.value
